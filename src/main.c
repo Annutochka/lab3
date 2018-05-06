66f206fe04a6a40eb925d208675714b36731d768
Anna <anutiykus@gmail.com>
2018-05-06 21:47:21 +0700

    Edit main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,15 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <string.h>
+#include "board_print_plain.h"
+#include "board.h"
 
 using namespace std;
 
 int main(){
 	char deck[8][8];
-
+	board_start(deck);
+	board_print(deck);
+	start_game(deck); 
 	return 0;
 }
