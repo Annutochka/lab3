ce98504667af829eeea6f2209c9dbb503884f0fb
Anna <anutiykus@gmail.com>
2018-05-06 20:49:04 +0700

    Create main.c file in src

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+	char deck[8][8];
+
+	return 0;
+}
