dc4b1ac69f34b2820bc5e4d27777b3eac45611df
Anna <anutiykus@gmail.com>
2018-05-09 19:17:42 +0700

    Edit board_print_plain.h

--- src/board_print_plain.h
+++ src/board_print_plain.h
@@ -1,7 +1,2 @@
-#ifndef BOARD_PRINT_PLAIN_H
-#define BOARD_PRINT_PLAIN_H
-
-void board_start(char deck[8][8]);
-void board_print(char deck[8][8]);
-
-#endif
+#define board_print_h_
+void board_print (char a[][9]);
