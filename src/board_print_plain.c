fcdfe3d8a8cfdf5e145f7e4921075aa7568a9af2
Anna <anutiykus@gmail.com>
2018-05-09 19:20:02 +0700

    Edit board_print_plain.c

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,35 +1,15 @@
-#include "board_print_plain.h"
-#include "board.h"
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-
-
-void board_start(char deck[8][8]) {
-	char letters[]={'r','n', 'b', 'q', 'k', 'b', 'n', 'r'};
-	for (int letter = 0; letter < 8; ++letter) {
-		deck[0][letter] = letters[letter];
-		deck[7][letter] = letters[letter]-32;
-		deck[1][letter] = 'p';
-		deck[6][letter] = 'p'-32;
-		for (int i = 2; i < 6; ++i) {
-			deck[i][letter] = ' ';
-		}
-	}
+void board_print (char a[][9])
+{
+    int i, j;
+    for (i=0; i<9; i++)
+    {
+        for (j=0; j<9; j++)
+        {
+            printf ("%c ", a[i][j]);
+        }
+        printf ("\n");
+    }
+    printf ("\n");
 }
-
-void board_print(char deck[8][8]) {
-	system("CLS");
-	printf("======== CHESS ========\n");
-	for (int number = 0; number < 8; ++number) {
-		printf("| %d | ", number+1);
-		for (int letter = 0; letter < 8; ++letter) {
-			printf("%c ",deck[number][letter]);
-		}
-		printf("|\n");
-	}
-	printf("|---------------------|\n");
-	printf("|   | A B C D E F J H |\n");
-	printf("-----------------------\n");
-} 
